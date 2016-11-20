a0f1ede1d5b1164dc3d75922a79cc32a15db92a5
dmrosen <drosen2000@gmail.com>
2016-11-20 15:13:42 -0800

    Adding a pair of functions to enable chordal initialization.

---
+++ MATLAB/SE-Sync/lib/chordal_initialization.m
@@ -0,0 +1,73 @@
+function [Rchordal, tchordal] = chordal_initialization(measurements)
+% function [Rchordal, tchordal] = chordal_initialization(measurements)
+%
+% This helper function accepts a MATLAB struct containing the raw 
+% measurements specifying a special Euclidean synchronization problem, and 
+% returns the corresponding chordal initialization for this problem.
+%
+% INPUT: A MATLAB struct 'measurements' containing the following fields
+% (see eq. (11) in the long-form version of the paper for details):
+% edges:  An (mx2)-dimension encoding the edges in the measurement network;
+%     edges(k, :) = [i,j] means that the kth measurement is of the
+%     relative transform from pose i to pose j.  NB:  This indexing scheme 
+%     requires that the states x_i are numbered sequentially as 
+%     x_1, ... x_n.
+% R:  An m-dimensional cell array whose kth element is the rotational part
+%     of the kth measurement
+% t:  An m-dimensional cell array whose kth element is the translational
+%     part of the kth measurement
+% kappa:  An m-dimensional cell array whose kth element gives the precision
+%     of the rotational part of the kth measurement. 
+% tau:  An m-dimensional cell array whose kth element gives the precision
+%     of the translational part of the kth measurement.
+
+% OUTPUT: 
+%  -Rchordal:  A d x dn block matrix Rchordal containing the estimating
+%     orientations.
+%  -tchordal [optional]:  A d x n block matrix containing the estimated
+%     positions.
+
+d = length(measurements.t{1});
+n = max(max(measurements.edges));
+m = size(measurements.edges, 1);
+
+[B1, B2, B3] = construct_B_matrices(measurements);
+
+
+% First, estimate the rotations using *only* the rotational observations
+Id = eye(d);
+Id_vec = Id(:);
+
+% Compute the constant vector cR induced by fixing the first orientation
+% estimate to be the identity Id.
+cR = B3(:, 1:d^2) * Id_vec;
+
+% Compute an estimate of the remaining rotations by solving the resulting
+% least squares problem *without* enforcing the constraint the the
+% estimates lie in SO(d)
+r2vec = - B3(:, d^2 + 1 : end) \ cR;
+rvec = vertcat(Id_vec, r2vec);
+R_LS = reshape(rvec, d, d*n);
+
+% Now reproject these estimates onto SO(d)
+Rchordal = zeros(d, d*n);
+for i = 1:n
+    Rchordal(:, d*(i-1) + 1 : d*i) = project_to_SOd(R_LS(:, d*(i-1) + 1 : d*i));
+end
+
+if nargout > 1
+    
+    % Solve for the translations in terms of the rotations
+    
+    % Constant vector induced by fixing R
+    cT = B2 * Rchordal(:);
+    
+    % Solve for t_2, ... t_n assuming that t_1 = 0.
+    
+    t2 = - B1(:, d + 1 : end) \ cT;
+    tvec = vertcat(zeros(d, 1), t2);
+    tchordal = reshape(tvec, d, n);
+end
+
+end
+
---
+++ MATLAB/SE-Sync/lib/construct_B_matrices.m
@@ -0,0 +1,112 @@
+function [B3, B2, B1, B] = construct_B_matrices(measurements)
+% function [B3, B2, B1, B] = construct_B_matrices( measurements )
+%
+% Given a measurement struct of the same format that the SE-Sync method
+% expects, this function computes and returns the (sparse) system matrices
+% B1, B2, B3, and B defined in the first section of the appendix
+% "Reformulating the estimation problem" in the SE-Sync paper
+
+d = length(measurements.t{1});  % Dimension of observations
+n = max(max(measurements.edges));  % Number of poses
+m = size(measurements.edges, 1);  % Number of measurements
+
+% B3 matrix:
+B3_nnz = (d^3 + d^2)*m;
+B3_rows = zeros(1, B3_nnz);
+B3_cols = zeros(1, B3_nnz);
+B3_vals = zeros(1, B3_nnz);
+
+for e = 1:m
+    tail = measurements.edges(e,1);
+    head = measurements.edges(e,2);
+    
+    sqkappa = sqrt(measurements.kappa{e});
+    Rt = measurements.R{e}';
+    
+    % Block entries corresponding to the tail of this edge
+    
+    for r = 1:d
+        for c = 1:d
+            
+            % Block representation of the -sqrt(kappa) *Rt(i,j) * I_d that
+            % appears in the Kronecker product
+            
+            idxs = [(d^3 + d^2)*(e-1) + d^2*(r-1) + d*(c - 1) + 1 : (d^3 + d^2)*(e-1) + d^2*(r-1) + d*(c - 1) + d];
+            
+            B3_rows(idxs) = [d^2 * (e-1) + d*(r-1) + 1 : d^2 * (e-1) + d*(r-1) + d];
+            B3_cols(idxs) = [d^2 * (tail - 1) + d*(c-1) + 1 : d^2 * (tail - 1) + d*(c-1) + d];
+            B3_vals(idxs) = -sqkappa * Rt(r,c) * ones(1, d);
+        end
+    end
+    
+    % Large d x d diagonal block entry corresponding to the head of this
+    % edge
+    
+    idxs = [d^3 * e + d^2*(e-1) + 1 : (d^3 + d^2)*e];
+    
+    B3_rows(idxs) = [d^2 *(e-1) + 1 : d^2 * e];
+    B3_cols(idxs) = [d^2 *(head -1) + 1 : d^2 * head];
+    B3_vals(idxs) = sqkappa * ones(1, d^2);
+end
+
+B3 = sparse(B3_rows, B3_cols, B3_vals, d^2*m, d^2*n);
+
+
+if nargout > 1
+    
+    % B2 matrix:
+    B2_nnz = m*d^2;
+    B2_rows = zeros(1, B2_nnz);
+    B2_cols = zeros(1, B2_nnz);
+    B2_vals = zeros(1, B2_nnz);
+    
+    for e = 1:m
+        tail = measurements.edges(e, 1);
+        
+        sqtau = sqrt(measurements.tau{e});
+        tij = measurements.t{e};
+        
+        
+        for idx = 1:d
+            % Block representation of -sqrt(tau)*t_ij(i) * I_d in the Kronecker
+            % product:
+            
+            B2_rows(d^2 * (e-1) + d*(idx-1) + 1 : d^2 * (e-1) + d*idx) = [d*(e-1) + 1 : d*e];
+            B2_cols(d^2 * (e-1) + d*(idx-1) + 1 : d^2 * (e-1) + d*idx) = [d^2 * (tail-1) + d*(idx - 1) + 1 : d^2 * (tail-1) + d*idx];
+            B2_vals(d^2 * (e-1) + d*(idx-1) + 1 : d^2 * (e-1) + d*idx) = -sqtau * tij(idx)*ones(1,d);
+        end
+    end
+    
+    B2 = sparse(B2_rows, B2_cols, B2_vals, d*m, d^2*n);
+    
+    
+    % B1 matrix:
+    
+    B1_nnz = 2*d*m;
+    B1_rows = zeros(1, B1_nnz);
+    B1_cols = zeros(1, B1_nnz);
+    B1_vals = zeros(1, B1_nnz);
+    
+    for e = 1:m
+        tail = measurements.edges(e,1);
+        head = measurements.edges(e,2);
+        
+        % Block entry corresponding to the tail of this edge
+        B1_rows(2*d*(e-1) + 1 : 2*d*e - d) = [d*(e-1) + 1 : d*e];
+        B1_cols(2*d*(e-1) + 1 : 2*d*e - d) = [d*(tail - 1) + 1 : d*tail];
+        B1_vals(2*d*(e-1) + 1 : 2*d*e - d) = -sqrt(measurements.tau{e})*ones(1, d);
+        
+        % Block entry corresponding to the head of this edge
+        B1_rows(2*d*e - d + 1 : 2*d*e) = [d*(e-1) + 1 : d*e];
+        B1_cols(2*d*e - d + 1 : 2*d*e) = [d*(head - 1) + 1 : d*head];
+        B1_vals(2*d*e - d + 1 : 2*d*e) = sqrt(measurements.tau{e})*ones(1, d);
+    end
+    
+    B1 = sparse(B1_rows, B1_cols, B1_vals, d*m, d*n);
+    
+end
+
+if nargout >= 4
+    B = [B1, B2;
+        sparse(size(B3, 1), size(B1, 2)), B3];
+end
