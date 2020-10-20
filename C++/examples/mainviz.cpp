4e32720ac2abf3fc07fd2e04fdb860919ad3f417
antonio teran <teran@mit.edu>
2020-10-20 02:21:23 -0400

    Add visualization executable for testing.

---
+++ C++/examples/mainviz.cpp
@@ -0,0 +1,36 @@
+#include "SESync/SESync.h"
+#include "SESync/SESyncVisualizer.h"
+#include "SESync/SESync_utils.h"
+
+using namespace std;
+using namespace SESync;
+
+bool write_poses = false;
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " [input .g2o file]" << endl;
+    exit(1);
+  }
+
+  size_t num_poses;
+  measurements_t measurements = read_g2o_file(argv[1], num_poses);
+  cout << "Loaded " << measurements.size() << " measurements between "
+       << num_poses << " poses from file " << argv[1] << endl
+       << endl;
+  if (measurements.size() == 0) {
+    cout << "Error: No measurements were read!"
+         << " Are you sure the file exists?" << endl;
+    exit(1);
+  }
+
+  SESyncOpts opts;
+  opts.verbose = true; // Print output to stdout
+  opts.num_threads = 4;
+  opts.initialization = Initialization::Random; // Make it interesting.
+  opts.log_iterates = true; // Want them for visualization!!!
+
+  // Run SE-Sync, and launch the visualization magic.
+  SESyncVisualizer viz(num_poses, measurements, opts);
+  viz.RenderWorld();
+}
